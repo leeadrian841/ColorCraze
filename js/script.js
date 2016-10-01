b1083e0a0bfa75e70ce3dd139a0a4d0adda6b883
leeadrian841 <leeadrian841@gmail.com>
2016-10-01 18:47:46 +0800

    new

--- js/script.js
+++ js/script.js
@@ -1,26 +1,59 @@
-$(document).ready(init() {
-  numberOfQuestions() {
+$(document).ready(function() {
+  var $body = $('body')
+  var $start = $('.start')
+  var $restart = $('.restart')
+  var $middle = $('.middle')
 
-  }
-  currentQuestion() {
+  var player1 = 0
+  var player2 = 0
+  var player1current = 0
+  var player2current = 0
+  var currentPlayer = 0
 
-  }
-  correctAnswer() {
+  $start.on('click', start)
+  $restart.on('click', restart)
 
-  }
-  numberOfChoices() {
+  function start() {
 
-  }
-  playTurn() {
+    var QuestionsArr = ['3 + 3 = 10']
+    function Questions() {
+      var $para = $('<p>')
+      $middle.append($para)
+      $para.text(QuestionsArr)
+    }
+    Questions()
+    function numberOfQuestions() {
 
-  }
-  isGameOver() {
+    }
+    function currentQuestion() {
 
-  }
-  whoWon() {
+    }
+    function correctAnswer() {
 
+    }
+    function numberOfChoices() {
+
+    }
+    function playTurn() {
+
+    }
+    function isGameOver() {
+
+    }
+    function whoWon() {
+      if (player1current === player2current) {
+        return 3 // Draw
+        alert ('This game is a draw! Please play again.')
+      } if (player1current > player2current) {
+        return 1 // Player 1 wins
+        alert ('Player 1 wins!')
+      } else {
+        return 2 // Player 2 wins
+        alert ('Player 2 wins!')
+      }
+    }
   }
-  restart() {
+  function restart() {
 
   }
 })
