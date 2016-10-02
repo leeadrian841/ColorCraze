cbd7f390257f84865f8dc536f943b8ac3c825712
leeadrian841 <leeadrian841@gmail.com>
2016-10-02 13:55:56 +0800

    new

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   var $body = $('body')
+  var $middle = $('.middle')
+  var $bottom = $('.bottom')
   var $start = $('.start')
   var $restart = $('.restart')
-  var $middle = $('.middle')
 
   var player1 = 0
   var player2 = 0
@@ -10,50 +11,35 @@ $(document).ready(function() {
   var player2current = 0
   var currentPlayer = 0
 
-  $start.on('click', start)
-  $restart.on('click', restart)
+  $start.on('click', startGame)
+  $restart.on('click', restartGame)
 
-  function start() {
+  var startGame = function() {
 
-    var QuestionsArr = ['3 + 3 = 10']
-    function Questions() {
-      var $para = $('<p>')
-      $middle.append($para)
-      $para.text(QuestionsArr)
-    }
-    Questions()
-    function numberOfQuestions() {
+    var $restart = $('<button type="button" class="btn btn-danger restart">Restart</button>')
+    $bottom.append($restart)
 
-    }
-    function currentQuestion() {
 
-    }
-    function correctAnswer() {
-
-    }
-    function numberOfChoices() {
-
-    }
-    function playTurn() {
-
-    }
-    function isGameOver() {
-
-    }
-    function whoWon() {
-      if (player1current === player2current) {
-        return 3 // Draw
-        alert ('This game is a draw! Please play again.')
-      } if (player1current > player2current) {
-        return 1 // Player 1 wins
-        alert ('Player 1 wins!')
-      } else {
-        return 2 // Player 2 wins
-        alert ('Player 2 wins!')
-      }
-    }
+  //   function playTurn() {
+  //
+  //   }
+  //   function isGameOver() {
+  //
+  //   }
+  //   function whoWon() {
+  //     if (player1current === player2current) {
+  //       return 3 // Draw
+  //       alert ('This game is a draw! Please play again.')
+  //     } if (player1current > player2current) {
+  //       return 1 // Player 1 wins
+  //       alert ('Player 1 wins!')
+  //     } else {
+  //       return 2 // Player 2 wins
+  //       alert ('Player 2 wins!')
+  //     }
+  //   }
   }
-  function restart() {
+  function restartGame() {
 
   }
 })
